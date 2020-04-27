2658cfa484aa04682e01ba183913b4db269f32a9
camrambo <49794860+camrambo@users.noreply.github.com>
2020-04-27 23:39:20 +0100

    Delete soon.php

--- shop/comingSoon/soon.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-	<div class="soonBox">
-		<h3 class="cs">Sorry, this product is not yet available.</h3>
-		<p class="cs">Please check back soon!</p>
-		<a href="/store/shop/" class="b2s"><-Back To Shop</a>
-	</div>
-</div>
